c2bdbcc0ef791915b28e5f3df599fa670e6f12fe
msaiko239 <48723529+msaiko239@users.noreply.github.com>
2020-05-21 09:49:38 -0500

    Update test_raemis.py

--- agi/test_raemis.py
+++ agi/test_raemis.py
@@ -1,9 +1,15 @@
 #importing the requests library
 import requests
+from configparser import ConfigParser
 import sys
 
+# getting config info from ini
+config = ConfigParser()
+config.read('/var/www/html/config.ini')
+HOST = config.get('Raemis_EPC_System', 'IP')
+
 # defining the api-endpoint
-API_ENDPOINT = "http://raemis:password@192.168.100.2/api/smsc_message?id=1"
+API_ENDPOINT = "http://raemis:password@" + HOST + "/api/smsc_message?id=1"
 
 # data to be sent to api
 data = {'to_msisdn':(sys.argv[1]),
@@ -19,3 +25,5 @@ r = requests.post(url = API_ENDPOINT, data = data)
 pastebin_url = r.text
 if '200' in str(r.status_code):
     print('Page accepted by Raemis')
+else:
+    print('Page Not Accepted By Raemis')
