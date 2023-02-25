1e964c88f122ac1387f227019e63c6c2f161d52b
Matt Saiko <48723529+msaiko239@users.noreply.github.com>
2023-02-24 22:51:24 -0600

    Update status.php
    Bug in reboot link

--- status.php
+++ status.php
@@ -98,21 +98,21 @@ $needle = "running";
 
 if (strpos($output, $needle) !==false) {
   echo '<img src="images/MSMservicerunning.jpg" />';
-  echo '<p>The MSM Service is Running</>';
-  echo '<a href="/restart.php" /><p> Click Here to Restart </>';
-  echo '<a href="/stop.php" /><p> Click Here to Stop </>';
-  echo '<a href="/reboot.php" /><p>Click Here to Reboot</>';
+  echo '<p>The MSM Service is Running</p>';
+  echo '<a href="/restart.php" /><p> Click Here to Restart </p>';
+  echo '<a href="/stop.php" /><p> Click Here to Stop </p>';
+  echo '<p><a href="/reboot.php" /> Click Here to Reboot</p></a>';
 } else {
-  echo '<a href="/start.php" /><img src="images/MSMservicestopped.jpg"/>';
-  echo '<p>The MSM Service is Stopped Click the Banner to Start</>';
-  echo '<a href="/reboot.php" /><p>Click Here to Reboot</>';
+  echo '<a href="/start.php" /><img src="images/MSMservicestopped.jpg"/p>';
+  echo '<p>The MSM Service is Stopped Click the Banner to Start</p>';
+  echo '<a href="/reboot.php" /><p>Click Here to Reboot</p></a>';
 }
 ?>
 </div>
 <br></br>
 
-  <div style="position: absolute; bottom: 10; left: 10; width: 10000px; text-align:left;">
-            Message Server Rev 1.2
+  <div style="position: absolute; bottom: 10; left: 10; width: 1000px; text-align:left;">
+           Message Server Rev 1.2
   </div>
 </br>
  </body>
