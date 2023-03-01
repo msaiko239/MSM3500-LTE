aa9d5b2a5d6bb901da3a65d8c27fa2b405fefb1e
Matt Saiko <48723529+msaiko239@users.noreply.github.com>
2023-03-01 15:21:39 -0600

    Update config.php
    Updating GUI css

--- config.php
+++ config.php
@@ -5,193 +5,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style_sheet.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
   </head>
+  <body>
+    <aside>
+      <p> Menu </p>
+      <a href="index.php">
+        <i class="fa fa-home" aria-hidden="true"></i>
+        Home
+      </a>
+      <a href="sendpage.php">
+        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        Send Message
+      </a>
+      <a href="status.php">
+        <i class="fa fa-check-square-o" aria-hidden="true"></i>
+        Service Status
+      </a>
+      <a href="input.php">
+        <i class="fa fa-file-text-o" aria-hidden="true"></i>
+        View Log
+      </a>
+      <a href="service.php">
+        <i class="fa fa-file" aria-hidden="true"></i>
+        Service Output
+      </a>
+    </aside>
+    <div style="margin-left:15%">
+      <div class="w3-container" id="bgimage">
+        <div style="padding-left:16px">
+        <img src="images/Raemisip.jpg">
+        </div>
+        <br>
+        </br>
+        <?php
 
-<body>
-<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-<style>
-aside {
-  color: #fff;
-  width: 250px;
-  padding-left: 20px;
-  height: 100vh;
-  background-image: linear-gradient(30deg , #0048bd, #44a7fd);
-  border-top-right-radius: 80px;
-  position: absolute;
-  left: 0;
-}
+        //put the file path here
+       $filepath = 'config.ini';
 
-aside a {
-  font-size: 19px;
-  color: #fff;
-  display: block;
-  padding: 12px;
-  padding-left: 30px;
-  text-decoration: none;
-  -webkit-tap-highlight-color:transparent;
-}
+        //after the form submit
 
-aside a:hover {
-  color: #3f5efb;
-  background: #fff;
-  outline: none;
-  position: relative;
-  background-color: #fff;
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-}
+        if($_POST){
+	        $data = $_POST;
+	        //update ini file, call function
+	        update_ini_file($data, $filepath);
+        }
 
-aside a i {
-  margin-right: 5px;
-}
+        //this is the function going to update your ini file
+	        function update_ini_file($data, $filepath) {
+	                $content = "";
 
-aside a:hover::after {
-  content: "";
-  position: absolute;
-  background-color: transparent;
-  bottom: 100%;
-  right: 0;
-  height: 35px;
-  width: 35px;
-  border-bottom-right-radius: 18px;
-  box-shadow: 0 20px 0 0 #fff;
-}
+	                //parse the ini file to get the sections
+	                //parse the ini file using default parse_ini_file() PHP function
+	                $parsed_ini = parse_ini_file($filepath, true);
 
-aside a:hover::before {
-  content: "";
-  position: absolute;
-  background-color: transparent;
-  top: 38px;
-  right: 0;
-  height: 35px;
-  width: 35px;
-  border-top-right-radius: 18px;
-  box-shadow: 0 -20px 0 0 #fff;
-}
+        	        foreach($data as $section=>$values){
+	                                //append the section
+	                                $content .= "[".$section."]\n";
+	                                //append the values
+	                                foreach($values as $key=>$value){
+	                                        $content .= $key."=".$value."\n";
+	                                }
+	                }
 
-aside p {
-  margin: 0;
-  padding: 40px 0;
-}
+        	        //write it into file
+	                if (!$handle = fopen($filepath, 'w')) {
+	                                return false;
+	                }
 
-body {
-  font-family: 'Roboto';
-  width: 100%;
-  height: 100vh;
-  margin: 0;
-}
-.social {
-  height: 0;
-}
+	                $success = fwrite($handle, $content);
+	                fclose($handle);
 
-.social i:before {
-  width: 14px;
-  height: 14px;
-  font-size: 14px;
-  position: fixed;
-  color: #fff;
-  background: #0077B5;
-  padding: 10px;
-  border-radius: 50%;
-  top:5px;
-  right:5px;
-}
-</style>
-<aside>
-  <p> Menu </p>
-  <a href="index.php">
-    <i class="fa fa-home" aria-hidden="true"></i>
-    Home
-  </a>
-  <a href="sendpage.php">
-    <i class="fa fa-envelope-o" aria-hidden="true"></i>
-    Send Message
-  </a>
-  <a href="status.php">
-    <i class="fa fa-check-square-o" aria-hidden="true"></i>
-    Service Status
-  </a>
-  <a href="input.php">
-    <i class="fa fa-file-text-o" aria-hidden="true"></i>
-    View Log
-  </a>
-  <a href="service.php">
-    <i class="fa fa-file" aria-hidden="true"></i>
-    Service Output
-  </a>
-</aside>
-
-<div style="margin-left:15%">
-<div class="w3-container" id="bgimage">
-
-<div style="padding-left:16px">
-<img src="images/Raemisip.jpg">
-</div>
-<br>
-</br>
-<div>
-<?php
-
-//put the file path here
-$filepath = 'config.ini';
-
-//after the form submit
-
-if($_POST){
-	$data = $_POST;
-	//update ini file, call function
-	update_ini_file($data, $filepath);
-}
-
-//this is the function going to update your ini file
-	function update_ini_file($data, $filepath) {
-	        $content = "";
-
-	        //parse the ini file to get the sections
-	        //parse the ini file using default parse_ini_file() PHP function
-	        $parsed_ini = parse_ini_file($filepath, true);
-
-	        foreach($data as $section=>$values){
-	                        //append the section
-	                        $content .= "[".$section."]\n";
-	                        //append the values
-	                        foreach($values as $key=>$value){
-	                                $content .= $key."=".$value."\n";
-	                        }
+        	        return $success;
+	        }
+        ?>
+        <div class="form">
+          <form action="" method="post" class="pads" id="my_form">
+	        <?php
+                $filepath = 'config.ini';
+                $parsed_ini = parse_ini_file($filepath, true);
+	        foreach($parsed_ini as $section=>$values){
+	                echo "<h3>$section</h3>";
+	                //keep the section as hidden text so we can update once the form submitted
+	                echo "<input type='hidden' value='$section' name='$section' />";
+                        $parsed_ini2 = parse_ini_file($filepath);
+                        $ip = ($parsed_ini2['IP']);
+                        $user = ($parsed_ini2['User']);
+                        $pass = ($parsed_ini2['Pass']);
+                        echo "<p> IP      :    <input type='text' name='{$section}[IP]' value='$ip' />"."</p>";
+                        echo "<p> User: <input type='text' name='{$section}[User]' value='$user' />"."</p>";
+                        echo "<p> Pass: <input type='text' name='{$section}[Pass]' value='' /> Cannot use ?{}|&~![()^\" "."</p>";
 	        }
 
-	        //write it into file
-	        if (!$handle = fopen($filepath, 'w')) {
-	                        return false;
-	        }
-
-	        $success = fwrite($handle, $content);
-	        fclose($handle);
-
-	        return $success;
-	}
-?>
-<div class="form">
-<form action="" method="post" class="pads" id="my_form">
-	<?php
-        $filepath = 'config.ini';
-        $parsed_ini = parse_ini_file($filepath, true);
-	foreach($parsed_ini as $section=>$values){
-	        echo "<h3>$section</h3>";
-	        //keep the section as hidden text so we can update once the form submitted
-	        echo "<input type='hidden' value='$section' name='$section' />";
-                $parsed_ini2 = parse_ini_file($filepath);
-                $ip = ($parsed_ini2['IP']);
-                $user = ($parsed_ini2['User']);
-                $pass = ($parsed_ini2['Pass']);
-                echo "<p> IP      :    <input type='text' name='{$section}[IP]' value='$ip' />"."</p>";
-                echo "<p> User: <input type='text' name='{$section}[User]' value='$user' />"."</p>";
-                echo "<p> Pass: <input type='text' name='{$section}[Pass]' value='' /> Cannot use ?{}|&~![()^\" "."</p>";
-	}
-
-	?>
-	<input type="submit" value="Update INI" onclick="alert('Configuration Updated')">
-</form>
-</div>
-
-</body>
+	        ?>
+	        <input type="submit" value="Update INI" onclick="alert('Configuration Updated')">
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
